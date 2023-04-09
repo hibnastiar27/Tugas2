e251c0bef9fdbc57d0d183dc2ea80b03482fe943
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2023-04-09 11:19:40 +0700

    add autoKetik

---
+++ js/autoketik.js
@@ -0,0 +1,27 @@
+// Inisialisasi variabel
+const words = document.querySelector(".left-main h1 span");
+
+let text = ["Beautiful", "Perfect", "Aesthetic", "Elegant"];
+let j = 0; // nilai index
+let i = 0; // nilai huruf dalam index
+
+function autoKetik() {
+  // memotong kata menjadi huruf
+  words.innerHTML = text[j].slice(0, i);
+  i++;
+
+  //  membatasi kata yang muncul
+  if (i > text[j].length) {
+    i = 0;
+
+    // membatasi nilai index
+    if (++j === text.length) {
+      j = 0;
+    }
+  }
+}
+
+// memanggil fungsi dalam interval yang ditetapkan
+setInterval(() => {
+  autoKetik();
+}, 250);
